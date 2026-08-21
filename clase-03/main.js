3fbae447c3cd4f8943078d206db91fc59ef81a3e
unknown <victor.villafane@davinci.edu.ar>
2026-08-21 10:53:22 -0300

    clase-03

---
+++ clase-03/main.js
@@ -0,0 +1,51 @@
+// const http = require("http") //CommonJS
+// const personajes = require("./data/personajes.js")
+// const page = require("./page/utils.js")
+import http from "http"
+import personajes from "./data/personajes.js"
+import page from "./page/utils.js"
+import { createPage, createList } from "./page/utils.js"
+import { readFile } from "fs/promises"
+
+const server = http.createServer((request, response) => {
+    console.log(request.url)
+    switch (request.url) {
+        case "/":
+            response.end(page.createPage("Bienvenido", "Victor Villafañe"))
+            break
+        case "/materia":
+            response.end(page.createPage("Materia", "Aplicaciones hibridas"))
+            break
+        case "/alumnos":
+            response.end(page.createPage("alumnos", page.createList(personajes)))
+            break
+        case "/pagina":
+            readFile("./public/index.html", "utf8")
+                .then(data => response.end(data))
+                .catch(err => response.end(page.createPage("Pagina no encontrada", "404")))
+            break
+        case "/homer-simpson.jpg":
+            readFile("./public/homer-simpson.jpg")
+                .then(data => response.end(data))
+                .catch(err => response.end(page.createPage("Pagina no encontrada", "404")))
+            break
+        case "/favicon.ico":
+            readFile("./public/homer-simpson.jpg")
+                .then(data => response.end(data))
+                .catch(err => response.end(page.createPage("Pagina no encontrada", "404")))
+            break
+        case "/style.css":
+            readFile("./public//style.css", "utf8")
+                .then(data => response.end(data))
+                .catch(err => response.end(page.createPage("Pagina no encontrada", "404")))
+            break
+        case "/profesor":
+            response.end(page.createPage("Profesor", "1234"))
+            break
+        default:
+            response.end(page.createPage("Pagina no encontrada", "404"))
+            break
+    }
+})
+
+server.listen(2026)
---
+++ clase-03/page/utils.js
@@ -0,0 +1,20 @@
+export function createPage(title, content){
+    let html = ""
+    html += `<!DOCTYPE html><html lang="es"><head><meta charset="UTF-8">`
+    html += `<title>${title}</title></head><body>`
+    html += `<h1>${title}</h1>`
+    html += content
+    html += "</body></html>"
+    return html
+}
+
+export function createList(lista){
+    let html = ""
+    html += "<ul>"
+    lista.forEach( (item) => html += "<li>Nombre: " + item.nombre + " Nota:" + item.nota + "</li>" )
+    html += "</ul>"
+    return html
+}
+
+// module.exports = { createPage, createList }
+export default { createPage, createList }
